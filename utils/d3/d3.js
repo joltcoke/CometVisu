c076b9eba4913e1510c3dfcd919bf06b0ff65ed9
Florian Schirmer <jolt@tuxbox.org>
2026-09-13 10:43:29 +0200

    Build the d3 bundle with the parts diagram2 needs
    The bundled library is assembled from an explicit export list, and diagram2
    uses six functions that were not on it: zoom and zoomIdentity for the pan and
    zoom gestures, pointers for multi-touch, mean to average them, axisRight for
    the second y axis and curveStepAfter for stepped series.
    
    Add them to utils/d3/d3.js, declare d3-zoom as a dependency and regenerate
    source/resource/libs/d3.min.js with npm run update-d3. The bundle grows from
    78 to 88 kB, which is far less than the full d3 build the plugin would need
    otherwise.

--- utils/d3/d3.js
+++ utils/d3/d3.js
@@ -1,12 +1,13 @@
 import { group, pointer, transition } from "d3";
-import { line, area, curveLinear, curveStep, curveNatural, stack, stackOrderNone, stackOffsetDiverging } from "d3-shape";
+import { line, area, curveLinear, curveStep, curveStepAfter, curveNatural, stack, stackOrderNone, stackOffsetDiverging } from "d3-shape";
 import { formatDefaultLocale, format } from "d3-format";
 import { timeFormatDefaultLocale, timeFormat } from "d3-time-format";
 import { scaleTime, scaleLinear, scaleUtc, scaleBand, scaleOrdinal } from "d3-scale";
-import { axisBottom, axisLeft } from 'd3-axis';
+import { axisBottom, axisLeft, axisRight } from 'd3-axis';
 import { timeParse } from "d3-time-format";
-import { select } from "d3-selection";
-import { extent, min, max, InternSet, least, map, range, groupSort, rollup, index, union } from "d3-array";
+import { select, pointers } from "d3-selection";
+import { zoom, zoomIdentity } from "d3-zoom";
+import { extent, min, max, mean, InternSet, least, map, range, groupSort, rollup, index, union } from "d3-array";
 import { timeDay, timeSecond, timeHour, timeMinute, timeMonday, timeMonth, timeYear, timeWeek, utcDay, utcSecond, utcHour, utcMinute, utcMonday, utcMonth, utcYear, utcWeek } from "d3-time";
 import { easeLinear } from "d3";
 // import {hsl, rgb, color} from 'd3-color';
@@ -20,6 +21,7 @@ export default {
   area: area,
   curveLinear: curveLinear,
   curveStep: curveStep,
+  curveStepAfter: curveStepAfter,
   curveNatural: curveNatural,
   stack: stack,
   stackOrderNone: stackOrderNone,
@@ -35,11 +37,16 @@ export default {
   scaleOrdinal,
   axisBottom: axisBottom,
   axisLeft: axisLeft,
+  axisRight: axisRight,
   timeParse: timeParse,
   select: select,
+  pointers: pointers,
+  zoom: zoom,
+  zoomIdentity: zoomIdentity,
   extent: extent,
   min: min,
   max: max,
+  mean: mean,
   InternSet: InternSet,
   least: least,
   map: map,
